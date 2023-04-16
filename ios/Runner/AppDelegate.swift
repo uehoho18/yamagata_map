7358b8997cb885e71b97c0349cec59ac54e8b1ac
uehoho18 <ramune276@gmail.com>
2023-04-16 15:49:50 +0900

    GoogleMapを表示

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("YOUR IOS API KEY HERE")
+    GMSServices.provideAPIKey("AIzaSyDs8r9snB7DX6nHAyxcQoLk8F2PtarCwwE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
