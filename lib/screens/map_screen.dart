2637f0ee6e7ffb6e7e612104f8f9e7dbdfa54f24
uehoho18 <ramune276@gmail.com>
2023-04-18 13:08:34 +0900

    Map箇所のリファクター

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
@@ -8,21 +12,31 @@ class MapScreen extends StatefulWidget {
   });
 
   @override
-  _MapScreenState createState() => _MapScreenState();
+  State<MapScreen> createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController mapController;
+  // 現在地を監視するためのStream
+  late StreamSubscription<Position> positionStream;
   Set<Marker> markers = {};
 
-  final CameraPosition initialCameraPosition = CameraPosition(
+  final CameraPosition initialCameraPosition = const CameraPosition(
     target: LatLng(35.681236, 139.767125),
     zoom: 16.0,
+  );
+
+  // 現在地通知の設定
+  final LocationSettings locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.high, //正確性:highはAndroid(0-100m),iOS(10m)
+    distanceFilter: 0,
   );
 
   @override
   void dispose() {
     mapController.dispose();
+    // Streamを閉じる
+    positionStream.cancel();
     super.dispose();
   }
 
@@ -35,6 +49,7 @@ class _MapScreenState extends State<MapScreen> {
           mapController = controller;
           await _requestPermission();
           await _moveToCurrentLocation();
+          _watchCurrentLocation();
         },
         myLocationButtonEnabled: false,
         markers: markers,
@@ -43,7 +58,6 @@ class _MapScreenState extends State<MapScreen> {
   }
 
   Future<void> _requestPermission() async {
-    // 位置情報の許可を求める
     LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       await Geolocator.requestPermission();
@@ -54,32 +68,58 @@ class _MapScreenState extends State<MapScreen> {
     LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.always ||
         permission == LocationPermission.whileInUse) {
-      // 現在地を取得
       final Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high,
       );
 
       setState(() {
-        markers.add(
-          Marker(
-            markerId: const MarkerId("current_location"),
-            position: LatLng(
-              position.latitude,
-              position.longitude,
-            ),
+        markers.add(Marker(
+          markerId: const MarkerId('current_location'),
+          position: LatLng(
+            position.latitude,
+            position.longitude,
           ),
-        );
+        ));
       });
 
+      await mapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: LatLng(position.latitude, position.longitude),
+            zoom: 16,
+          ),
+        ),
+      );
+    }
+  }
+
+  void _watchCurrentLocation() {
+    // 現在地を監視
+    positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((position) async {
+      // マーカーの位置を更新
+      setState(() {
+        markers.removeWhere(
+            (marker) => marker.markerId == const MarkerId('current_location'));
+
+        markers.add(Marker(
+          markerId: const MarkerId('current_location'),
+          position: LatLng(
+            position.latitude,
+            position.longitude,
+          ),
+        ));
+      });
       // 現在地にカメラを移動
       await mapController.animateCamera(
         CameraUpdate.newCameraPosition(
           CameraPosition(
             target: LatLng(position.latitude, position.longitude),
-            zoom: 16.0,
+            zoom: 16,
           ),
         ),
       );
-    }
+    });
   }
 }
