a4a59d8755e530e15ed2174212613732e2ebce83
uehoho18 <ramune276@gmail.com>
2023-04-24 02:10:30 +0900

    値を追加

--- lib/components/auth_modal/components/auth_text_form_field.dart
+++ lib/components/auth_modal/components/auth_text_form_field.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
 
 class AuthTextFormField extends StatelessWidget {
-  const AuthTextFormField({super.key});
+  const AuthTextFormField({
+    super.key,
+    this.controller,
+    this.onChanged,
+    this.validator,
+    this.labelText = '',
+    this.obscureText = false,
+  });
+
+  final TextEditingController? controller;
+  final void Function(String)? onChanged;
+  final String? Function(String?)? validator;
+  final String labelText;
+  final bool obscureText;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: validator,
+      obscureText: obscureText,
+      onChanged: onChanged,
       decoration: InputDecoration(
-        border: const OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(24)),
-        ),
-      ),
+          border: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(24)),
+          ),
+          labelText: labelText),
     );
   }
 }
