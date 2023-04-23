fce567e7e81e2f8a677e01602da976a501b168e7
uehoho18 <ramune276@gmail.com>
2023-04-24 02:45:08 +0900

    テキストフォームまで実装

---
+++ lib/components/auth_modal/components/sign_up_form.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:yamagata_map/components/auth_modal/components/auth_text_form_field.dart';
+
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({
+    super.key,
+  });
+
+  @override
+  State<SignUpForm> createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  // ---------  Validation ---------
+
+  String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter some text';
+    }
+    return null;
+  }
+
+  String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter some text';
+    }
+    return null;
+  }
+
+  String? validateConfirmPassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter some text';
+    }
+    // パスワードテキストフィールドの内容と照合
+    if (value != _passwordController.text) {
+      return 'Password does not match';
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text(
+            'Sign Up', // 変更
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          AuthTextFormField(
+            controller: _emailController,
+            validator: validateEmail,
+            labelText: 'Email',
+          ),
+          const SizedBox(height: 16.0),
+          AuthTextFormField(
+            controller: _passwordController,
+            obscureText: true,
+            validator: validatePassword,
+            labelText: 'Password',
+          ),
+          const SizedBox(height: 16.0),
+          AuthTextFormField(
+            obscureText: true,
+            validator: validateConfirmPassword,
+            labelText: 'Confirm Password',
+          ),
+          const SizedBox(height: 16.0),
+          SubmitButton(
+            labelName: '新規登録', // 変更
+            onTap: () => _submit(context),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _submit(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {}
+  }
+}
